4eb1b38c6d97f4091bdc4a4ed5692065e6de0889
marta <marta.voitanowych@gmail.com>
2017-05-10 13:04:51 +0300

    add menu and toast

--- app/src/main/java/com/maltseva/marta/githubquery/MainActivity.java
+++ app/src/main/java/com/maltseva/marta/githubquery/MainActivity.java
@@ -1,15 +1,36 @@
 package com.maltseva.marta.githubquery;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     EditText mSearchBoxEditText;
     TextView mUrlDisplayTextView;
     TextView mSearchResults;
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int menuItemThatWasSelected = item.getItemId();
+        if (menuItemThatWasSelected == R.id.action_search){
+            Context context = MainActivity.this;
+            String message = "Search clicked";
+            Toast.makeText(context,message,Toast.LENGTH_LONG).show();
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main,menu);
+        return true;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
