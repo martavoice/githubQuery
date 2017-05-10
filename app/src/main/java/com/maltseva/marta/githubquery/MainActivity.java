895161701161fe5085d61dccacb79982763ce8a1
marta <marta.voitanowych@gmail.com>
2017-05-10 12:24:19 +0300

    add variables

--- app/src/main/java/com/maltseva/marta/githubquery/MainActivity.java
+++ app/src/main/java/com/maltseva/marta/githubquery/MainActivity.java
@@ -2,12 +2,22 @@ package com.maltseva.marta.githubquery;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText mSearchBoxEditText;
+    TextView mUrlDisplayTextView;
+    TextView mSearchResults;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mSearchBoxEditText = (EditText)findViewById(R.id.et_search_box);
+        mUrlDisplayTextView = (TextView) findViewById(R.id.tv_url_display);
+        mSearchResults = (TextView) findViewById(R.id.tv_github_search_result_json);
     }
 }
